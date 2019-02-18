28d67924005c7fee88104a15ca422025d772fc22
Gardner Vickers <gardner@vickers.me>
2019-02-18 00:03:12 -0800

    Rework ShutdownableThread state transitions to be more explicit
    using an enumeration and use Condition vars for notifying threads
    of thread state changes instead of CountdownLatch.

--- core/src/main/scala/kafka/utils/ShutdownableThread.scala
+++ core/src/main/scala/kafka/utils/ShutdownableThread.scala
@@ -17,7 +17,8 @@
 
 package kafka.utils
 
-import java.util.concurrent.{CountDownLatch, TimeUnit}
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.{Condition, Lock, ReentrantLock}
 
 import org.apache.kafka.common.internals.FatalExitError
 
@@ -25,50 +26,95 @@ abstract class ShutdownableThread(val name: String, val isInterruptible: Boolean
         extends Thread(name) with Logging {
   this.setDaemon(false)
   this.logIdent = "[" + name + "]: "
-  private val shutdownInitiated = new CountDownLatch(1)
-  private val shutdownComplete = new CountDownLatch(1)
+
+
+  private object ThreadStates extends Enumeration {
+    type ThreadState = Value
+    val Created: ThreadStates.Value = Value(0)
+    val Running: ThreadStates.Value = Value(1)
+    val ShuttingDown: ThreadStates.Value = Value(2)
+    val Shutdown: ThreadStates.Value = Value(3)
+  }
+
+  private var threadState: ThreadStates.Value = ThreadStates.Created
+  private val threadStateLock: Lock = new ReentrantLock()
+  private val shuttingDownCondition: Condition = threadStateLock.newCondition()
+  private val shutdownCondition: Condition = threadStateLock.newCondition()
+  private def withLock[T](lock: Lock)(f: => T): T = {
+    lock.lock()
+    try f
+    finally lock.unlock()
+  }
+
+   /**
+    * Set the current thread state, ensuring that we follow the linear progression from
+    * Created -> Running -> ShuttingDown -> Shutdown. If a valid state transition occurs that involves a state being
+    * waited on, the relevant Condition's are signaled.
+    *
+    * Returns a tuple of (oldState, newState), which can be used to check if any progress was made.
+    * In the event that an invalid state transition is attempted, this method returns a tuple of (oldState, oldState)
+    * to signify nothing was changed.
+    *
+    */
+  private def setThreadState(newState: ThreadStates.Value): (ThreadStates.Value, ThreadStates.Value) = withLock(threadStateLock) {
+    if (newState >= threadState) {
+      val oldState = threadState
+      threadState = newState
+      newState match {
+        case ThreadStates.ShuttingDown =>
+          shuttingDownCondition.signalAll()
+        case ThreadStates.Shutdown =>
+          shuttingDownCondition.signalAll()
+          shutdownCondition.signalAll()
+        case _ =>
+      }
+      (oldState, newState)
+    } else {
+      (threadState, threadState)
+    }
+  }
+
 
   def shutdown(): Unit = {
     initiateShutdown()
     awaitShutdown()
   }
 
-  def isShutdownComplete: Boolean = {
-    shutdownComplete.getCount == 0
-  }
-
-  def initiateShutdown(): Boolean = {
-    this.synchronized {
-      if (isRunning) {
-        info("Shutting down")
-        shutdownInitiated.countDown()
-        if (isInterruptible)
-          interrupt()
-        true
-      } else
-        false
-    }
+  def isShutdownComplete: Boolean = withLock(threadStateLock) {
+    threadState == ThreadStates.Shutdown
   }
 
   /**
-   * After calling initiateShutdown(), use this API to wait until the shutdown is complete
+   * Initiate thread shutdown. If the thread has been started, any threads waiting on
+   * ShutdownableThread.pause will be signaled. The thread will be interrupted if isInterruptible
+   * is true. If the thread has not been started, returns false. If the thread was previously running, returns true.
    */
-  def awaitShutdown(): Unit = {
-    if (this.isAlive)
-      shutdownComplete.await()
+  def initiateShutdown(): Boolean = {
+    val (oldState, newState) = setThreadState(ThreadStates.ShuttingDown)
+    if (isInterruptible)
+      interrupt()
+    oldState == ThreadStates.Running
+  }
+
+  /**
+   *  After calling initiateShutdown(), use this API to wait until the shutdown is complete. If the thread was not
+   *  started or has already finished shutting down, this method returns immediately.
+   */
+  def awaitShutdown(): Unit = withLock(threadStateLock) {
+    if (ThreadStates.Running <= threadState && threadState < ThreadStates.Shutdown)
+      shutdownCondition.await()
     info("Shutdown completed")
   }
 
   /**
-   *  Causes the current thread to wait until the shutdown is initiated,
-   *  or the specified waiting time elapses.
-   *
-   * @param timeout
-   * @param unit
+   *  Causes the current thread to wait until shutdown is initiated or the specified waiting time elapses.
+   *  Blocks the caller thread even if this thread has not been started.
    */
-  def pause(timeout: Long, unit: TimeUnit): Unit = {
-    if (shutdownInitiated.await(timeout, unit))
-      trace("shutdownInitiated latch count reached zero. Shutdown called.")
+  def pause(timeout: Long, unit: TimeUnit): Unit = withLock(threadStateLock) {
+    if (threadState < ThreadStates.ShuttingDown) {
+      shuttingDownCondition.await(timeout, unit)
+      trace("Shutdown initiated")
+    }
   }
 
   /**
@@ -76,27 +122,27 @@ abstract class ShutdownableThread(val name: String, val isInterruptible: Boolean
    */
   def doWork(): Unit
 
+  def isRunning: Boolean = withLock(threadStateLock) {
+    threadState == ThreadStates.Running
+  }
+
+
   override def run(): Unit = {
+    setThreadState(ThreadStates.Running)
     info("Starting")
     try {
       while (isRunning)
         doWork()
     } catch {
       case e: FatalExitError =>
-        shutdownInitiated.countDown()
-        shutdownComplete.countDown()
-        info("Stopped")
+        setThreadState(ThreadStates.Shutdown)
         Exit.exit(e.statusCode())
       case e: Throwable =>
         if (isRunning)
           error("Error due to", e)
     } finally {
-       shutdownComplete.countDown()
+      setThreadState(ThreadStates.Shutdown)
     }
-    info("Stopped")
-  }
-
-  def isRunning: Boolean = {
-    shutdownInitiated.getCount() != 0
+    info("ShutDown")
   }
 }
--- core/src/test/scala/unit/kafka/utils/ShutdownableThreadTest.scala
+++ core/src/test/scala/unit/kafka/utils/ShutdownableThreadTest.scala
@@ -19,8 +19,8 @@ package kafka.utils
 import java.util.concurrent.{CountDownLatch, TimeUnit}
 
 import org.apache.kafka.common.internals.FatalExitError
-import org.junit.{After, Test}
 import org.junit.Assert.{assertEquals, assertTrue}
+import org.junit.{After, Test}
 
 class ShutdownableThreadTest {
 
@@ -29,26 +29,130 @@ class ShutdownableThreadTest {
 
   @Test
   def testShutdownWhenCalledAfterThreadStart(): Unit = {
-    @volatile var statusCodeOption: Option[Int] = None
-    Exit.setExitProcedure { (statusCode, _) =>
-      statusCodeOption = Some(statusCode)
-      // Sleep until interrupted to emulate the fact that `System.exit()` never returns
-      Thread.sleep(Long.MaxValue)
-      throw new AssertionError
+    val shutdownLatch = new CountDownLatch(1)
+    try {
+      @volatile var statusCodeOption: Option[Int] = None
+      Exit.setExitProcedure { (statusCode, _) =>
+        statusCodeOption = Some(statusCode)
+        // Sleep until interrupted to emulate the fact that `System.exit()` never returns
+        shutdownLatch.await()
+        throw new AssertionError
+      }
+      val latch = new CountDownLatch(1)
+      val thread = new ShutdownableThread("shutdownable-thread-test") {
+        override def doWork(): Unit = {
+          latch.countDown()
+          throw new FatalExitError
+        }
+      }
+      thread.start()
+      assertTrue("doWork was not invoked", latch.await(10, TimeUnit.SECONDS))
+
+      thread.shutdown()
+      TestUtils.waitUntilTrue(() => statusCodeOption.isDefined, "Status code was not set by exit procedure")
+      assertEquals(1, statusCodeOption.get)
+    } finally {
+      // allow thread to shutdown at the end of the test
+      shutdownLatch.countDown()
     }
-    val latch = new CountDownLatch(1)
+  }
+
+  @Test
+  def testInterrupt(): Unit =  {
+    val startupLatch = new CountDownLatch(1)
+    val shutdownLatch = new CountDownLatch(1)
+    try {
+      @volatile var caughtInterrupt = false
+      val thread = new ShutdownableThread("shutdownable-thread-test") {
+        override def doWork(): Unit = {
+          startupLatch.countDown()
+          try {
+            shutdownLatch.await()
+          } catch {
+            case e: InterruptedException =>
+              caughtInterrupt = true
+              throw e
+          }
+        }
+      }
+      thread.start()
+      startupLatch.await(10, TimeUnit.SECONDS)
+      assertEquals("Interrupt should not have been triggered yet", caughtInterrupt, false)
+
+      thread.shutdown()
+      thread.awaitShutdown()
+      assertEquals("Expected interrupt to be caught", caughtInterrupt, true)
+
+    } finally {
+      // release the thread if it hasn't already been released
+      shutdownLatch.countDown()
+    }
+  }
+
+  @Test
+  def testAwaitingOnShutdown(): Unit = {
+
+    val threadStartedLatch = new CountDownLatch(1)
     val thread = new ShutdownableThread("shutdownable-thread-test") {
-      override def doWork: Unit = {
-        latch.countDown()
-        throw new FatalExitError
+      override def doWork(): Unit = {
+        threadStartedLatch.countDown()
+        while (true) {
+          Thread.sleep(100)
+        }
       }
     }
     thread.start()
-    assertTrue("doWork was not invoked", latch.await(10, TimeUnit.SECONDS))
+    threadStartedLatch.await(10, TimeUnit.SECONDS)
 
-    thread.shutdown()
-    TestUtils.waitUntilTrue(() => statusCodeOption.isDefined, "Status code was not set by exit procedure")
-    assertEquals(1, statusCodeOption.get)
+    val shutdownInitiated = new CountDownLatch(1)
+    val shutdownInitiatedThread = new Thread() {
+      override def run(): Unit = {
+        thread.pause(10, TimeUnit.SECONDS)
+        shutdownInitiated.countDown()
+      }
+    }
+    shutdownInitiatedThread.start()
+
+    val shutdownCompleted = new CountDownLatch(1)
+    val awaitShutdownThread = new Thread() {
+      override def run(): Unit = {
+        thread.awaitShutdown()
+        shutdownCompleted.countDown()
+      }
+    }
+    awaitShutdownThread.start()
+
+    assertEquals(shutdownInitiated.getCount, 1)
+    assertEquals(shutdownCompleted.getCount, 1)
+
+
+    assertTrue(thread.initiateShutdown())
+    shutdownInitiated.await(10, TimeUnit.SECONDS)
+    assertEquals(shutdownInitiated.getCount, 0)
+
+
+    thread.awaitShutdown()
+    shutdownCompleted.await(10, TimeUnit.SECONDS)
+    assertEquals(shutdownCompleted.getCount, 0)
+
   }
 
+  @Test
+  def testNonStartedThread(): Unit = {
+    val thread = new ShutdownableThread("shutdownable-thread-test") {
+      override def doWork(): Unit = {
+      }
+    }
+
+    val awaitShutdownLatch = new CountDownLatch(1)
+    val awaitShutdownThread = new Thread() {
+      override def run(): Unit = {
+        thread.awaitShutdown()
+        awaitShutdownLatch.countDown()
+      }
+    }
+    awaitShutdownThread.start()
+    awaitShutdownLatch.await(10, TimeUnit.SECONDS)
+    assertEquals("Expected awaitShutdown() to return without blocking", awaitShutdownLatch.getCount, 0)
+  }
 }
